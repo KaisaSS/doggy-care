e3436b8e23117d96931c7b8e22d391e737e30e07
Kaisa <parvej@Kaisas-MacBook-Pro.local>
2021-10-25 12:41:14 +0100

    Add banner img

--- src/App.js
+++ src/App.js
@@ -2,22 +2,23 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import bookDaycare from "./components/booking/bookDaycare";
 import Booking from "./components/booking/Booking";
 import bookWalk from "./components/booking/bookWalk";
-import Contact from "./components/Contact";
-import Home from "./components/Home";
-import Navbar from "./components/Navbar";
+import Contact from "./components/contact/Contact";
+import Home from "./components/home/Home";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App"></div>
-      <Navbar />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/booking" component={Booking} />
-        <Route path="/booking/walk" component={bookWalk} />
-        <Route path="/booking/daycare" component={bookDaycare} />
-        <Route path="/contact" component={Contact} />
-      </Switch>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/booking" component={Booking} />
+          <Route path="/booking/walk" component={bookWalk} />
+          <Route path="/booking/daycare" component={bookDaycare} />
+          <Route path="/contact" component={Contact} />
+        </Switch>
+      </div>
     </BrowserRouter>
   );
 }
---
+++ src/components/home/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+// Styles
+import "./home.css";
+
+const Home = () => {
+  return (
+    <div>
+      <div className="banner">
+        <div className="slogan">Caring for your pet when you can't</div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
